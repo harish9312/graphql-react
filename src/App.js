f6524cae221c1a7022666dd94cecedc5796660ca
Harish Soni <hksoni.9312@gmail.com>
2019-04-22 16:35:45 +0530

    Added project files, wired up to the graphql endpoint.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
+import ApolloClient from 'apollo-boost';
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+import { Users } from './Users';
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql"
+})
 
 class App extends Component {
   render() {
+    console.log('>> client', client);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client} >
+        <div className="App">
+        <Users/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
